e7498588fbf04f886a22d07827336a5d78f17275
harshit.gupta1 <harshit.gupta@atmecs.com>
2020-12-30 13:20:45 +0530

    enable mailing flag

--- src/config.js
+++ src/config.js
@@ -3,10 +3,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
     PORT: process.env.PORT, // e.g. 3000
-    Maintenance: process.env.Maintenance,       // Is website under maintenance ? boolean
+    Maintenance: process.env.Maintenance,       // Is website under maintenance ? String(boolean)
     EMAIL_SENDER: process.env.EMAIL_SENDER,     // SES sender email address
     MONGODB_URL: process.env.MONGODB_URL,       // MongoDB database URL
     MONGODB_NAME: process.env.MONGODB_NAME,     // MongoDB database name
+    MAILING_ENABLED: process.env.MAILING_ENABLED, // ENABLE or DISABLE SES ? String(boolean)
     
     // SECRETS
     JWT_SECRET: process.env.JWT_SECRET,         // A secret for generating JWTs for users.
--- src/emails/account.js
+++ src/emails/account.js
@@ -1,11 +1,11 @@
 const ses = require('node-ses')
 
-const {SES_API_KEY, SES_API_SECRET, EMAIL_SENDER, SES_REGION} = require('../config')
+const {SES_API_KEY, SES_API_SECRET, EMAIL_SENDER, SES_REGION, MAILING_ENABLED} = require('../config')
 
 const client = ses.createClient({key: SES_API_KEY, secret: SES_API_SECRET, amazon: `https://email.${SES_REGION}.amazonaws.com`});
 
 const sendWelcomeEmail = (email, name) => {
-    client.sendEmail({
+    MAILING_ENABLED === 'true' && client.sendEmail({
         from: EMAIL_SENDER,
         replyTo: EMAIL_SENDER,
         to: email,
@@ -20,7 +20,7 @@ const sendWelcomeEmail = (email, name) => {
 }
 
 const sendGoodbyeEmail = (email, name) => {
-    client.sendEmail({
+    MAILING_ENABLED === 'true' && client.sendEmail({
         from: EMAIL_SENDER,
         replyTo: EMAIL_SENDER,
         to: email,
